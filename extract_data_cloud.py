cfc1b99dc43b60a0e83e8019764d11d6aa0b64f7
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2024-12-15 12:36:10 +0530

    Update extract_data_cloud.py

--- extract_data_cloud.py
+++ extract_data_cloud.py
@@ -15,20 +15,17 @@ processed_count = 0
 
 blobs = list(bucket.list_blobs())
 
-# Process only the first video
 for blob in blobs:
     if blob.name.endswith(('.mp4', '.avi', '.mov')):
-        # Construct the local file path
+      
         local_file_path = os.path.join(download_path, blob.name)
         os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
 
         print(f"Downloading {blob.name}...")
         blob.download_to_filename(local_file_path)
 
-        print(f"Processing {local_file_path}...")
-        # Add your video processing logic here if needed
 
         processed_count += 1
-        break  # Exit the loop after processing the first video
+        break  
 
 print(f"Total videos processed: {processed_count}")

887b5d3b1111711b26a9134557d838135f0750db
zenitsu0509 <gangwarsanu8@gmail.com>
2025-03-17 04:18:33 +0000

    update code

--- annote-entent/italian.py
+++ annote-entent/italian.py
@@ -163,7 +163,7 @@ def process_jsonl_file(input_path, output_path, batch_size=10):
     print(f"\nProcessing complete. Output saved to: {output_path}")
 
 if __name__ == "__main__":
-    input_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Data-store/italian/italian_processed.jsonl"
-    output_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/gemini_metadata/italian.jsonl"
+    input_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/gemini_metadata/europeen/cv/italain_cv.jsonl"
+    output_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/gemini_metadata/europeen/cv/italain_cv_gem.jsonl"
 
     process_jsonl_file(input_jsonl_path, output_jsonl_path)

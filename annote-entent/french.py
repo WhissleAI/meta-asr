69ddf90e23586e941da6356cc5fde68046d915a2
zenitsu0509 <gangwarsanu8@gmail.com>
2025-03-22 03:36:18 +0000

    update path to annote french cv

--- annote-entent/french.py
+++ annote-entent/french.py
@@ -163,7 +163,7 @@ def process_jsonl_file(input_path, output_path, batch_size=10):
     print(f"\nProcessing complete. Output saved to: {output_path}")
 
 if __name__ == "__main__":
-    input_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/ner_metadata/french.jsonl"
-    output_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/gemini_metadata/french.jsonl"
+    input_jsonl_path = "/external4/datasets/jsonl_data/euro/french_cv.jsonl"
+    output_jsonl_path = "/hydra2-prev/home/compute/workspace_himanshu/Processed_Data/gemini_metadata/europeen/cv/french_cv.jsonl"
 
     process_jsonl_file(input_jsonl_path, output_jsonl_path)
